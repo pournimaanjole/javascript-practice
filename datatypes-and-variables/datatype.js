90ceaac59153b2e5da887d9e73ecf876c9f0d722
pournimaanjole <pournimaanjole@gmail.com>
2023-09-24 15:23:17 +0530

    initial commit

---
+++ datatypes-and-variables/datatype.js
@@ -0,0 +1,8 @@
+// number data means integer number or float number
+let num1=76; 
+let num2=87.98;
+// string data type 
+let name="rani";
+let sirname='anjole'
+// boolean data typle it gives the andswer in true or false 
+
---
+++ datatypes-and-variables/templatesring.js
@@ -0,0 +1,3 @@
+let age =21;
+let name='rani';
+console.log(`hey there my name is &{name} and i am &{age} years old`);
